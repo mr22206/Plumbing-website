97ca15405a3e0e3c093b9b4e4121ae0d067c9174
mohammad <mohammad.rezki@viacesi.fr>
2024-11-25 16:09:21 +0100

    Mise à jour :  PCS Plomberie website

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Urgences from './pages/Urgences';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navbar />
         <Routes>
